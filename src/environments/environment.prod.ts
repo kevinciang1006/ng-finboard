03b10b91fff4232b0e5dc27d798f11e6a7493f73
Kevin Ciang <kevin.ciang@organisationsolutions.com>
2026-03-20 11:57:38 +0700

    feat: update production API URL in environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'YOUR_RAILWAY_URL', // TODO: replace with Railway deployment URL
+  apiUrl: 'ng-finboard-production.up.railway.app', // TODO: replace with Railway deployment URL
 };
